8e1c6dfcee34b37f11feebebd9c220b21230fe32
Ambrus Tóth <ping@ambrus.dev>
2025-10-25 08:15:16 +0300

    Custom navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+    integrations: [
+        starlight({
+            title: 'hylo',
+            logo: {
+                replacesTitle: true,
+                dark: './src/assets/hylo-blue.png',
+                light: './src/assets/hylo-black.png',
+            },
+            social: [
+                {icon: 'github', label: 'GitHub', href: 'https://github.com/hylo-lang/hylo'},
+                {
+                    icon: 'slack',
+                    label: 'Slack',
+                    href: 'https://join.slack.com/t/val-qs97696/shared_invite/zt-1z3dsblrq-y4qXfEE6wr6uMEJSN9uFyg'
+                },
+            ],
+            components: {
+                'Header': './src/components/Header.astro',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        {label: 'Example Guide', slug: 'guides/example'},
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: {directory: 'reference'},
+                },
+            ],
+            plugins: [starlightThemeRapide()],
+        })
+        ,
+    ],
+})
+;
