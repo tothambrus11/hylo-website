c7574700822ef76db89053d29f4ea45098ae71eb
Ambrus Tóth <ping@ambrus.dev>
2025-10-26 00:00:20 +0300

    add talk and paper structure

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import {defineCollection, z} from 'astro:content';
 import {docsLoader} from '@astrojs/starlight/loaders';
 import {docsSchema} from '@astrojs/starlight/schema';
-import {glob} from "astro/loaders";
+import {glob, file} from "astro/loaders";
 
 export const collections = {
     docs: defineCollection({loader: docsLoader(), schema: docsSchema()}),
@@ -12,6 +12,30 @@ export const collections = {
             description: z.string().optional(),
             date: z.date(),
             author: z.string().optional(),
+        })
+    }),
+    papers: defineCollection({
+        // loader: file("src/content/papers.json"),
+        loader: file("src/content/papers.yml"),
+        // loader: glob({pattern: "**/*.{md,mdx}", base: "src/content/papers/"}),
+        schema: z.object({
+            title: z.string(),
+            authors: z.array(z.string()),
+            venue: z.string(),
+            year: z.number(),
+            month: z.number().optional(),
+            url: z.string().url().optional(),
+        })
+    }),
+    talks: defineCollection({
+        loader: file("src/content/talks.yml"),
+        schema: z.object({
+            title: z.string(),
+            authors: z.array(z.string()),
+            venue: z.string(),
+            year: z.number(),
+            month: z.number().optional(),
+            url: z.string().url().optional(),
         })
     })
 };
--- src/content/docs/.sidebar.ts
+++ src/content/docs/.sidebar.ts
@@ -1,20 +1,13 @@
 import {type StarlightUserConfig} from "@astrojs/starlight/types";
 import {guidesSidebar} from "./docs/guides/.sidebar.ts";
+import {referencesSidebar} from "./docs/reference/.sidebar.ts";
 
 type Sidebar = StarlightUserConfig['sidebar'];
 
-const outdatedBade = { badge: { text: 'outdated', variant: 'caution' } } as const;
 
 export const sidebar: Sidebar = [
     guidesSidebar,
-    {
-        label: 'Reference',
-        // autogenerate: {directory: 'docs/reference'},
-        items: [
-            { label: 'IR', link: 'https://docs.hylo-lang.org/hylo-ir', ...outdatedBade },
-            { label: 'Language Specification', slug: 'docs/reference/specification', ...outdatedBade },
-        ]
-    },
+    referencesSidebar,
 ]
 
 
