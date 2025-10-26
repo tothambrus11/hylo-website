b9c153531fad3430a2c0c082850b5c70eff05d52
Ambrus Tóth <ping@ambrus.dev>
2025-10-26 17:45:22 +0200

    Add language tour

--- src/content/docs/docs/user/.sidebar.ts
+++ src/content/docs/docs/user/.sidebar.ts
@@ -7,6 +7,17 @@ export const userGuideSidebar: ArrayElement<Sidebar> = {
     label: 'User Guide',
     items: [
         {label: 'Getting Started', slug: 'docs/user/compiling-hylo'},
-        {label: 'Beginner\'s Introduction to Hylo', slug: 'docs/user/beginners-introduction'},
+        {
+            label: 'Language Tour', items: [
+                {label: 'Introduction', slug: 'docs/user/language-tour'},
+                {label: 'Hello, World!', slug: 'docs/user/language-tour/hello-world'},
+                {label: 'Modules', slug: 'docs/user/language-tour/modules'},
+                {label: 'Bindings', slug: 'docs/user/language-tour/bindings'},
+                {label: 'Basic types', slug: 'docs/user/language-tour/basic-types'},
+                {label: 'Functions and methods', slug: 'docs/user/language-tour/functions-and-methods'},
+                {label: 'Subscripts', slug: 'docs/user/language-tour/subscripts'},
+                {label: 'Concurrency', slug: 'docs/user/language-tour/concurrency'}
+            ]
+        },
     ]
 };
