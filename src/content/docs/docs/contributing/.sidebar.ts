6e039a2d020cc615ba6290d1f611e5065ceca516
Ambrus Tóth <ping@ambrus.dev>
2025-11-10 00:33:26 +0200

    Improve first impressions

--- src/content/docs/docs/contributing/.sidebar.ts
+++ src/content/docs/docs/contributing/.sidebar.ts
@@ -7,10 +7,9 @@ export const contributingGuideSidebar: ArrayElement<Sidebar> = {
     label: 'Contributing Guide',
     items: [
         {label: 'Introduction', slug: 'docs/contributing'},
-        {label: 'How can you help?', slug: 'docs/contributing/open-topics'},
         {label: 'Building the Compiler', slug: 'docs/contributing/building-the-compiler'},
-        {label: 'Editing this Documentation', slug: 'docs/contributing/documentation'},
         {label: 'Research Collaboration', slug: 'docs/contributing/research-collab'},
+        {label: 'Editing this Documentation', slug: 'docs/contributing/documentation'},
         {label: 'Diversity and Inclusion', slug: 'docs/contributing/diversity-and-inclusion'},
         {label: 'Code of Conduct', slug: 'docs/contributing/code-of-conduct'},
     ],
